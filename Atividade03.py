69cbdd60095167f4b5efca98c266d8ce2c5feb31
Matheus Amendola <matheusamendolaa@gmail.com>
2023-05-12 11:16:49 +0000

    Atividade 03 pronta

---
+++ Atividade03.py
@@ -0,0 +1,17 @@
+import requests
+from rich import print
+
+key = "7b229feabe44cf5326591114"
+
+# Taxa de Cambio
+
+def get_cambio(moeda):
+    r = requests.get(f"https://v6.exchangerate-api.com/v6/{key}/pair/USD/{moeda}")
+    return r.json()
+
+
+Taxas = ["BRL", "EUR", "JPY"]
+
+for cambio in range(len(Taxas)):
+    resultado = get_cambio(Taxas[cambio])
+    print(f"A taxa de [bold]{Taxas[cambio]}[/bold] para USD é de: {resultado['conversion_rate']}")
